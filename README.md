d9163033c5fd52bad8b7c226ebb7eb0fdad214fb
Jamie Yap <jamieyap@users.noreply.github.com>
2023-10-12 19:13:01 -0400

    Upload code for CORE data pipeline.

---
+++ README.md
@@ -0,0 +1,17 @@
+<!---
+---
+output:
+  pdf_document: default
+urlcolor: magenta
+---
+--->
+
+# CORE-MARS
+
+## About this Repository
+
+This repository contains a data pipeline that produces a core dataset that may be used for many types of subsequent data analyses relating to the Mobile Assistance for Regulating Smoking (MARS) Micro-Randomized Trial (MRT).
+
+## Protocol for the MARS MRT
+
+Nahum-Shani, I., Potter, L. N., Lam, C. Y., Yap, J., Moreno, A., Stoffel, R., ... & Wetter, D. W. (2021). The mobile assistance for regulating smoking (MARS) micro-randomized trial design protocol. Contemporary clinical trials, 110, 106513.
---
+++ prepare-analytic-datasets/create-basic-analysis-dataset.R
@@ -0,0 +1,40 @@
+source("paths.R")
+source("prepare-analytic-datasets/functions-for-analytical-dataset-creation.R")
+library(tidyverse)
+
+################################################################################
+# Load datasets
+################################################################################
+# This dataset contains data from only those sequences which began
+# within the 10-day MRT period.
+dat_matched_to_decision_points <- readRDS(file = file.path(path_manipulated_data, "dat_matched_to_decision_points.rds"))
+
+################################################################################
+# The following participants were excluded from analyses:
+# - Pilot participants
+# - Among participants who have any data from sequences which began within the
+#   10-day MRT period, those participants who did not complete
+#   at least 3 EMA between the 2nd and 9th day inclusive.
+#
+# Note:  This subset of the data may be used if the proximal outcome of
+#        interest was assessed via EMAs.
+################################################################################
+mars_ids_excluded_from_all_analytic_datasets <- readRDS(file = file.path(path_manipulated_data, "mars_ids_excluded_from_all_analytic_datasets.rds"))
+
+dat_mars_for_ema_assessed_proximal_outcome <- dat_matched_to_decision_points %>% filter(!(mars_id %in% mars_ids_excluded_from_all_analytic_datasets))
+
+saveRDS(dat_mars_for_ema_assessed_proximal_outcome, file = file.path(path_manipulated_data, "dat_mars_for_ema_assessed_proximal_outcome.rds"))
+
+################################################################################
+# The following participants were excluded from analyses:
+# - Pilot participants
+#
+# Note: This subset of the data may be used if the proximal outcome of
+#       interest was NOT assessed via EMAs, e.g., app usage.
+################################################################################
+mars_ids_pilot <- readRDS(file = file.path(path_manipulated_data, "mars_ids_pilot.rds"))
+
+dat_mars_for_nonema_assessed_proximal_outcome <- dat_matched_to_decision_points %>% filter(!(mars_id %in% mars_ids_pilot))
+
+saveRDS(dat_mars_for_nonema_assessed_proximal_outcome, file = file.path(path_manipulated_data, "dat_mars_for_nonema_assessed_proximal_outcome.rds"))
+
